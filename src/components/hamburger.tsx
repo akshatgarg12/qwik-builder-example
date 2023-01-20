b43aa6ed048a9a57c879863b15769c577e85f70f
akshatgarg12 <akshatarungarg78@gmail.com>
2023-01-20 13:50:21 +0530

    changed import path

--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -1,6 +1,6 @@
 import { component$} from '@builder.io/qwik';
 import {  RegisteredComponent } from '@builder.io/sdk-qwik';
-import { Counter, TableApp } from '~/integrations/react/mui';
+import { Counter, TableApp } from '../integrations/react/mui';
 
 export const apiKey = '8335d18816304315aebeb7e9532281ce'; // Enter your key here
 
