55be4c366a2374d1bc1bea328288779cbc0d90c6
akshatgarg12 <akshatarungarg78@gmail.com>
2023-01-20 13:43:25 +0530

    removed routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
-import { CUSTOM_COMPONENTS } from "~/components/hamburger";
 
 export const BUILDER_PUBLIC_API_KEY = '8335d18816304315aebeb7e9532281ce' // <-- Add your Public API KEY here
 export const BUILDER_MODEL = "page";
@@ -27,7 +26,6 @@ export default component$(() => {
           model={BUILDER_MODEL}
           content={content}
           apiKey={BUILDER_PUBLIC_API_KEY}
-          customComponents = {CUSTOM_COMPONENTS}
         />
       )}
     />
