232c8bf04e3a105aa91a00f394f8317d7bd81d4a
akshatgarg12 <akshatarungarg78@gmail.com>
2023-01-20 13:47:16 +0530

    changed import path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
-import { CUSTOM_COMPONENTS } from "~/components/hamburger";
+import { CUSTOM_COMPONENTS } from "../components/hamburger";
 
 export const BUILDER_PUBLIC_API_KEY = '8335d18816304315aebeb7e9532281ce' // <-- Add your Public API KEY here
 export const BUILDER_MODEL = "page";
