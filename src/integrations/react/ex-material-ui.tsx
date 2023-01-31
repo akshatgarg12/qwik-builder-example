20280804650381ef2b07f49627b01449addd8ea0
akshatgarg12 <akshatarungarg78@gmail.com>
2023-01-31 23:53:22 +0530

    removed unused export

--- src/integrations/react/ex-material-ui.tsx
+++ src/integrations/react/ex-material-ui.tsx
@@ -1,27 +1,6 @@
 /** @jsxImportSource react */
-import {
-    Divider, 
-    IconButton,
-    Tooltip,
-    SvgIcon, 
-    CircularProgress,
-    InputAdornment,
-    List,
-    ListItem,
-    ListItemText,
-    Paper,
-    TextField, 
-} from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { qwikify$ } from '@builder.io/qwik-react';
 
-export const MUIDivider = qwikify$(Divider)
-export const MUIIconButton = qwikify$(IconButton)
-export const MUITooltip = qwikify$(Tooltip)
-export const MUISvgIcon = qwikify$(SvgIcon)
+
 export const MUICircularProgress = qwikify$(CircularProgress, {clientOnly:true})
-export const MUIInputAdornment = qwikify$(InputAdornment)
-export const MUIList = qwikify$(List)
-export const MUIListItem = qwikify$(ListItem)
-export const MUIListItemText = qwikify$(ListItemText)
-export const MUIPaper = qwikify$(Paper)
-export const MUITextField = qwikify$(TextField)
