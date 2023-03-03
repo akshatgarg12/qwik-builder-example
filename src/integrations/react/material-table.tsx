de5b3aba38ea02f9c9c4fe5c36034917a441d7ab
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-03 15:42:43 +0530

    resolve material-table error

--- src/integrations/react/material-table.tsx
+++ src/integrations/react/material-table.tsx
@@ -1,35 +1,13 @@
 /** @jsxImportSource react */
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { qwikify$ } from '@builder.io/qwik-react';
 
-export interface MaterialTableProps {
-  headColumns: { label: string; numeric: boolean }[];
-  bodyRows: { columns: { content: any[]; numeric: boolean }[] }[];
-//   builderBlock: any;
-}
-
-export const MaterialTableComponent = (props: MaterialTableProps) => {
-  const { headColumns, bodyRows, children } = props;
+export const MaterialTableComponent = () => {
   return (
     <Table className="builder-table">
-      {headColumns && !!headColumns.length && (
-        <TableHead>
-          {headColumns.map((col, index) => (
-            <TableCell key={index} align={col.numeric ? 'right' : undefined}>
-              {col.label}
-            </TableCell>
-          ))}
-        </TableHead>
-      )}
-      {bodyRows && !!bodyRows.length && (
-        <TableBody>
-          {children}
-        </TableBody>
-      )}
+      <h1>Table</h1>
     </Table>
   );
 }
