8df4e9467f9be51c20eca76663a5e3f29acb162c
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-03 15:49:36 +0530

    removed react integrations

--- src/components/widgets.tsx
+++ src/components/widgets.tsx
@@ -1,25 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import { RegisteredComponent } from "@builder.io/sdk-qwik";
-import { QwikifyBasicTooltip } from "~/integrations/react/tooltip";
-import BuilderAccordion, { AccordionProps } from '~/integrations/react/accordion'
-import { accordionConfig } from "~/config/Accordion.config";
-const BasicTooltip = component$(() => <QwikifyBasicTooltip />)
-const Accordion = component$((props : AccordionProps) => <BuilderAccordion {...props}/>)
 const Name = component$(() => <h1>MY NAME</h1>)
 export const WIDGETS: RegisteredComponent[] = [
   {
-    component : BasicTooltip,
-    name : "Basic tooltip",
-    builtIn: true
-  },
-  {
     component: Name,
-    name : 'name',
-    builtIn:true
-  },
-  {
-    component : Accordion,
-    builtIn : true,
-    ...accordionConfig
+    name : 'name'
   }
 ];
--- src/integrations/react/ex-material-ui.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsxImportSource react */
-import {
-    Tooltip,
-    CircularProgress,
-} from '@material-ui/core';
-import { qwikify$ } from '@builder.io/qwik-react';
-
-export const MUITooltip = qwikify$(Tooltip)
-export const MUICircularProgress = qwikify$(CircularProgress, {clientOnly:true})
