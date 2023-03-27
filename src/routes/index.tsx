ddeb2c30a13ce8e9b401b92deb8e790e0b489286
akshatgarg12 <akshatarungarg78@gmail.com>
2023-03-27 15:14:17 +0530

    changed builder api key

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,8 @@ import {
 } from "@builder.io/sdk-qwik";
 import { WIDGETS } from "../components/widgets";
 
-// export const BUILDER_PUBLIC_API_KEY = "8335d18816304315aebeb7e9532281ce"; // <-- Add your Public API KEY here
-export const BUILDER_PUBLIC_API_KEY = "26950364a825464593a7fc11c6bbda89"; // <-- Add your Public API KEY here
+export const BUILDER_PUBLIC_API_KEY = "8335d18816304315aebeb7e9532281ce"; // <-- Add your Public API KEY here
+// export const BUILDER_PUBLIC_API_KEY = "26950364a825464593a7fc11c6bbda89"; // <-- Add your Public API KEY here
 export const BUILDER_MODEL = "page";
 export default component$(() => {
   const location = useLocation();
