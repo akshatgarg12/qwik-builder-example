9aa3b8b72cf9644c46e0f422e200aefa96903b12
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-13 21:40:10 +0530

    update sdks

---
+++ src/routes/square/index.tsx
@@ -0,0 +1,13 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <div style={{height:'100vh', display:'flex'}}>
+    <div style={{height:"200px", width:"200px", background : "red", margin:"auto", padding:"30px"}}>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis adipisci alias tempore! In rerum mollitia blanditiis laudantium repellendus iste hic ut laboriosam ratione distinctio ducimus dolores, quod consectetur dolorum quis.
+        </p>
+    </div>
+  </div>
+  );
+});
