c3d8f92946897dcaec4e1b48cba4adc13125d21d
akshatgarg12 <akshatarungarg78@gmail.com>
2023-04-19 14:35:12 +0530

    added header-testing page

--- src/routes/square/index.tsx
+++ src/routes/square/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <div style={{height:'100vh', display:'flex'}}>
-    <div style={{height:"200px", width:"200px", background : "red", margin:"auto", padding:"30px"}}>
+    <div style={{height:"200px", width:"200px", background : "#8F57BF", margin:"auto", padding:"40px", color:'#F3E8FF'}}>
         <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis adipisci alias tempore! In rerum mollitia blanditiis laudantium repellendus iste hic ut laboriosam ratione distinctio ducimus dolores, quod consectetur dolorum quis.
         </p>
