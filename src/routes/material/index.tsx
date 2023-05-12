ccab162feaed45f275df16c76fc749a0fabf820d
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-12 11:58:59 +0530

    changed material ui component

--- src/routes/material/index.tsx
+++ src/routes/material/index.tsx
@@ -1,25 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import {
-  MUIList,
-  MUIListItem,
-  MUIListItemText,
-} from "../../integrations/react/material-ui";
+import { MUITextField } from "../../integrations/react/material-ui";
 
 const MaterialPage = component$(() => {
   return (
     <div>
       <h1>Material Page</h1>
-      <MUIList>
-        <MUIListItem>
-          <MUIListItemText primary="Primary" />
-        </MUIListItem>
-        <MUIListItem>
-          <MUIListItemText primary="Secondary" />
-        </MUIListItem>
-        <MUIListItem>
-          <MUIListItemText primary="Third" />
-        </MUIListItem>
-      </MUIList>
+      <MUITextField label="Material Text Field" />
     </div>
   );
 });
