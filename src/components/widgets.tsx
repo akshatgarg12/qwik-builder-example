b9bf4183911f0f4962d0a485d8f64781e816b29e
akshatgarg12 <akshatarungarg78@gmail.com>
2023-06-08 11:33:03 +0530

    register builder widgets

--- src/components/widgets.tsx
+++ src/components/widgets.tsx
@@ -1,9 +1,36 @@
 import { component$ } from "@builder.io/qwik";
 import { RegisteredComponent } from "@builder.io/sdk-qwik";
-const Name = component$(() => <h1>MY NAME</h1>)
+import {
+  Accordion,
+  Carousel,
+  MasonryComponent,
+  Tabs,
+} from "../../fake_modules/qwik";
+import { accordionConfig } from "../config/Accordion.config";
+import { carouselConfig } from "../config/Carousel.config";
+import { tabsConfig } from "../config/builder-tabs.config";
+import { masonryConfig } from "../config/Masonry.config";
+
+const Name = component$(() => <h1>MY NAME</h1>);
 export const WIDGETS: RegisteredComponent[] = [
   {
     component: Name,
-    name : 'name'
-  }
+    name: "name",
+  },
+  {
+    component: Accordion,
+    ...accordionConfig,
+  },
+  {
+    component: Carousel,
+    ...carouselConfig,
+  },
+  {
+    component: MasonryComponent,
+    ...masonryConfig,
+  },
+  {
+    component: Tabs,
+    ...tabsConfig,
+  },
 ];
